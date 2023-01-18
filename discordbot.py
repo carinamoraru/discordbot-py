724332bf231c4d2b1f8575ef86a91d511b611565
carinamoraru <108097833+carinamoraru@users.noreply.github.com>
2023-01-18 16:43:01 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,19 +12,13 @@ COURIER_TOKEN = os.environ['COURIER_TOKEN']
 RECEIVE_EMAIL = os.environ['RECEIVE_EMAIL']
 CHANNEL_ID = 1063880677436690552
 MAX_SESSION_TIME_MINUTES = 1
-PC_COUNT = 2
 courier_client = Courier(auth_token=COURIER_TOKEN)
-RECEIVE_EMAIL = "mariamoraru0709@gmail.com"
 
 pcStatusList = []
 isSentList = []
 timeList = []
 timeTemp = datetime.now().strftime("%H%M%S")
 now = int(str(timeTemp)[0:2])*3600 + int(str(timeTemp)[2:4])*60 + int(str(timeTemp)[4:6])
-for i in range(PC_COUNT):
-    pcStatusList.append(0)
-    isSentList.append(0)
-    timeList.append(now)
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -32,52 +26,46 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 async def on_ready():
     print('Hello, Checking bot is ready!')
     channel = client.get_channel(CHANNEL_ID)
-    client.loop.create_task(watch())
-    courier_client.send_message(
-        message={
-            "to": {
-                "email": RECEIVE_EMAIL
-            },
-            "content": {
-                "title": "Request from Discord",
-                "body": "computer{{pc}} is wrong!"
-            },
-            "data": {
-                "pc": 0
-            }
-        }
-    )
+    client.loop.create_task(watch())    
     await channel.send("Hello, Checking bot is ready!")
 
-async def watch():
-    pcNumber = 1
+async def watch():    
     channel = client.get_channel(CHANNEL_ID)
 
     while True:
-        bound = MAX_SESSION_TIME_MINUTES * 10
+        bound = MAX_SESSION_TIME_MINUTES * 60
         intTime = bound
         while intTime > 2:
             async for message in channel.history(limit=1):
                 timeTemp = datetime.now().strftime("%H%M%S")
                 now = int(str(timeTemp)[0:2]) * 3600 + int(str(timeTemp)[2:4]) * 60 + int(str(timeTemp)[4:6])
-                # curDate = int(str(message.created_at.time())[0:2]) * 3600 + \
-                #           int(str(message.created_at.time())[3:5]) * 60 + int(str(message.created_at.time())[6:8])
-                if message.content.endswith('is working'):
+                if message.content.endswith('is working') and message.content.startswith('pc'):
                     pcNumber = int(message.content.replace('is working', '').replace('pc', '').strip())
-                    if pcStatusList[pcNumber] == 0:
-                        pcStatusList[pcNumber] = 1
-                        timeList[pcNumber] = now
-                        isSentList[pcNumber] = 0
+                    if pcNumber in pcNumberList:
+                        order = orderFunction(pcNumber, pcNumberList)
+                        if pcStatusList[order] == 0:
+                            pcStatusList[order] = 1
+                            timeList[order] = now
+                            isSentList[order] = 0
+                        else:
+                            pcStatusList[order] = 2
+                            timeList[order] = now
+                            isSentList[order] = 0
                     else:
-                        pcStatusList[pcNumber] = 2
-                        timeList[pcNumber] = now
-                        isSentList[pcNumber] = 0
+                        pcNumberList.append(pcNumber)
+                        pcStatusList.append(1)
+                        timeList.append(now)
+                        isSentList.append(0)
+                try:
                     await message.delete()
-                    # await channel.purge(limit=1)
+                except:
+                    pass
+                # await channel.purge(limit=1)
 
             await asyncio.sleep(1)
-            intTime -= 1
+            intTime -= 1           
 
+        # get element that exceed specified time
         order = 0
         timeTemp = datetime.now().strftime("%H%M%S")
         now = int(str(timeTemp)[0:2]) * 3600 + int(str(timeTemp)[2:4]) * 60 + int(str(timeTemp)[4:6])
@@ -99,16 +87,42 @@ async def watch():
         for pcStatus in pcStatusList:
             if pcStatus == 0 and isSentList[order] == 0:
                 isSentList[order] = 1
-                print("computer" + str(order) + " Was Stopped")
+                # send_email("computer" + str(pcNumberList[order]) + " Was Stopped")
+                print("computer" + str(pcNumberList[order]) + " Was Stopped")
             if pcStatus == 1 and isSentList[order] == 0:
                 isSentList[order] = 1
-                print("computer" + str(order) + " Was Started")
+                # send_email("computer" + str(pcNumberList[order]) + " Was Started")
+                print("computer" + str(pcNumberList[order]) + " Was Started")
             if pcStatus == 2:
                 isSentList[order] = 0
             order += 1
 
+def orderFunction(item, l, n=0):
+    if l:
+        if l[0] == item:
+            return n
+        elif len(l) >= 2:
+            return orderFunction(item, l[1:], n+1)
+        
 def is_me(m):
     return m.author == client.user
+
+def send_email(str):
+    result = courier_client.send_message(
+        message={
+            "to": {
+                "email": RECEIVE_EMAIL
+            },
+            "content": {
+                "title": "Request from Discord",
+                "body": "{{pc}}!"
+            },
+            "data": {
+                "pc": str
+            }
+        }
+    )
+    return result
 
 try:
     client.run(TOKEN)
